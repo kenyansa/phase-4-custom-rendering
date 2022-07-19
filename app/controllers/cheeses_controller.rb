f142862ff8f5c20d2dc6c77c2f52d27c13814630
Kenyansa <kenyansajustine@gmail.com>
2022-07-19 08:29:07 +0300

    completed and understood the code veeeeery well

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,18 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id, :name, :price, :is_best_seller]
   end
 
-  # GET /cheeses/:id
+  # GET /cheeses/:id - sieving out what we don't want with custom rendering
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: {error: 'Cheese Onge buana'}, status: 404
+      #alternatively, the status can be written as status:not_found
+    end
   end
 
 end
